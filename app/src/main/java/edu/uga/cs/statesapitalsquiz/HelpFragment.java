8f6e0abfc637417b5e767a06880f1a3accc20ae0
nehaau2305 <nehaau2305@gmail.com>
2025-10-28 10:14:28 -0400

    made help fragment & set up main activity to navigate to another activity which is a fragment holder. fragment will either be quiz, history, or help

--- app/src/main/java/edu/uga/cs/statesapitalsquiz/HelpFragment.java
+++ app/src/main/java/edu/uga/cs/statesapitalsquiz/HelpFragment.java
@@ -28,20 +28,19 @@ public class HelpFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    /**
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
         return inflater.inflate( R.layout.fragment_help, container, false );
-    }*/
+    }
 
     @Override
     public void onViewCreated( @NonNull View view, Bundle savedInstanceState ) {
         super.onViewCreated(view,savedInstanceState);
 
-        //TextView textView = getView().findViewById( R.id.textView3 );
-        //String text = getResources().getString( R.string.help_fragment_text );
-        //textView.setText( HtmlCompat.fromHtml( text, HtmlCompat.FROM_HTML_MODE_LEGACY ) );
+        TextView textView = getView().findViewById( R.id.helpTextView );
+        String text = getResources().getString( R.string.help_fragment_text );
+        textView.setText( HtmlCompat.fromHtml( text, HtmlCompat.FROM_HTML_MODE_LEGACY ) );
     }
 }
--- app/src/main/java/edu/uga/cs/statesapitalsquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/statesapitalsquiz/MainActivity.java
@@ -1,14 +1,24 @@
 package edu.uga.cs.statesapitalsquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    // define variables
+    Button startQuizB;
+    Button viewHistoryB;
+    Button helpB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        // connect buttons to appropriate UI components
+        startQuizB = findViewById(R.id.button);
+        viewHistoryB = findViewById(R.id.button2);
+        helpB = findViewById(R.id.button3);
+        // button click listener
+        ButtonClickListener listener = new ButtonClickListener();
+        startQuizB.setOnClickListener(listener);
+        viewHistoryB.setOnClickListener(listener);
+        helpB.setOnClickListener(listener);
+    } //onCreate
+
+    private class ButtonClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            Fragment fragment = null;
+            Intent intent = new Intent(MainActivity.this, FragmentHostActivity.class)
+
+            // Create a new fragment based on the used selection in the nav drawer
+            switch(v.getId()) {
+                case R.id.button:
+                    intent.putExtra("fragment type", "quiz");
+                    break;
+                case R.id.button2:
+                    intent.putExtra("fragment type", "history");
+                    break;
+                case R.id.button3:
+                    intent.putExtra("fragment type", "help");
+                    break;
+            }
+            startActivity(intent);
+
+        } //onClick
+    } //ButtonClickListener
 }
